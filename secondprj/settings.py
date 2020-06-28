d9fc381cdf0ef66873a3be8d0ed79a53ac429dfc
wooob12 <woobiboy1995@gmail.com>
2020-06-28 20:37:54 +0900

    heroku deploy

--- secondprj/settings.py
+++ secondprj/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zdtngq9#=a_p^!r9m_o$mb!62k^a**&&)1num_)6k$t#1!4-!9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'zdtngq9#=a_p^!r9m_o$mb!62k^a**&&)1num_)6k$t#1!4-!9')
+# SECRET_KEY = 'zdtngq9#=a_p^!r9m_o$mb!62k^a**&&)1num_)6k$t#1!4-!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+# Heroku 배포를 위한 추가
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
